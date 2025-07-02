60588d6b0c357be82a522299090be2e1a74caa53
electro-oz <shah.agam@gmail.com>
2025-07-02 17:06:57 +1000

    globals.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,1 @@
-import "./globals.css";
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "EOz - Your Smart Home Solution",
-  description: "Smart home automation products in Australia.",
-};
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
+import './globals.css'

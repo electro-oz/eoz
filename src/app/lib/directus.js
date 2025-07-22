f773765dc0f938e7df38b5136daa7cf99a64923c
electro-oz <shah.agam@gmail.com>
2025-07-22 18:47:49 +1000

    Update directus.js

--- src/app/lib/directus.js
+++ src/app/lib/directus.js
@@ -1,6 +1,6 @@
 export async function fetchHeaderMenu() {
   const url = process.env.NEXT_PUBLIC_DIRECTUS_URL + '/items/menu?deep[menu_items]=submenu';
-  console.log('✅ [fetchHeaderMenu] Using URL:', url); // this is key for debugging
+  console.log('✅ [fetchHeaderMenu] Using URL:', url); // this is key for debugging ... ... 
 
   try {
     const res = await fetch(url, { cache: 'no-store' });

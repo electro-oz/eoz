fbffcf12fffa4b595f0d5e0234e54c36961d88bf
electro-oz <shah.agam@gmail.com>
2025-07-22 18:14:05 +1000

    Update directus.js

--- src/app/lib/directus.js
+++ src/app/lib/directus.js
@@ -1,13 +1,12 @@
 // src/lib/directus.js
 export async function fetchHeaderMenu() {
-  const res = await fetch(
-    process.env.NEXT_PUBLIC_DIRECTUS_URL +
-      '/items/menu?deep[menu_items]=submenu',
-    { cache: 'no-store' }
-  );
+  const url = process.env.NEXT_PUBLIC_DIRECTUS_URL + '/items/menu?deep[menu_items]=submenu';
+  console.log('Fetching-from-front:' , url);
+  const res = await fetch(url, { cache: 'no-store' });
 
-  if (!res.ok) throw new Error('Failed to fetch menu');
-
+  if (!res.ok) {
+    throw new Error('Failed to fetch menu: ' + res.status + ' ' + res.statusText);
+  }
   const json = await res.json();
-  return json && json.data && json.data[0]; // Get the first menu entry
+  return json && json.data && json.data[0];
 }
